45e220b8cb66d811f953fcd823cf9829960e59e7
imprintnext-karma <kenton@imprintnext.com>
2026-08-13 13:54:06 +0530

    CI/CD workflow

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("HELLO WORLD");
+  return res.send("HELLO WORLD FROM NODEJS BACKEND");
 });
 
 
